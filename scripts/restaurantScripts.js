16c9760f13decd1fec2ebf4d96d2e8e89b4f8269
Seth Harbin <47837530+SethHarbin@users.noreply.github.com>
2019-03-02 16:53:12 -0600

    Add files via upload

--- scripts/restaurantScripts.js
+++ scripts/restaurantScripts.js
@@ -1,8 +1,15 @@
+/*
+Created by: Seth Harbin
+Created on: 3/2/2019
+Last Edited on: 3/2/2019
+*/
+
 function contactApprove(){
 	clearErrors();
 	alert("Information is valid and has not been submitted.");
 	return false;
 }
+
 function clearErrors() {    
     for (var i = 0; i < document.forms["contactUs"].elements.length; i++) {
         if (document.forms["contactUs"].elements[i].parentElement.className.indexOf("has-") != -1) {
